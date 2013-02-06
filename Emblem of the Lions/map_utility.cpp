f47107e60bff3fc2d2a78b060b0c40ba2d0b64df
Kayne Ruse <ratstail91@yahoo.com.au>
2013-02-06 22:28:55 +1100

    What the fuck am I doing anyway?

--- Emblem of the Lions/map_utility.cpp
+++ Emblem of the Lions/map_utility.cpp
@@ -1,9 +1,11 @@
 #include "map_utility.h"
 
-#include <string.h>
+#include <string>
+#include <vector>
 #include <fstream>
 
 #include <stdexcept>
+#include <iostream>
 
 MapUtility::MapUtility() {
 	tname[0] = '\0';
@@ -16,8 +18,8 @@ MapUtility::~MapUtility() {
 }
 
 void MapUtility::LoadMap(const char* fname) {
-	//open the map file
-	if (data == NULL)
+	//open the map file, check for errors
+	if (data != NULL)
 		DeleteMap();
 
 	std::ifstream is(fname);
@@ -26,9 +28,8 @@ void MapUtility::LoadMap(const char* fname) {
 		throw(std::runtime_error("Failed to load map data"));
 
 	//load the tileset
-	Uint16 w, h;
-
 	is.getline(tname, 512);
+	Uint16 w, h;
 	is >> w; is >> h;
 
 	tileset.LoadSurface(tname);
@@ -36,20 +37,44 @@ void MapUtility::LoadMap(const char* fname) {
 	tileset.SetClipW(w);
 	tileset.SetClipH(h);
 
-	//load and generate the array
+	//load the region information
 	is >> xCount;
 	is >> yCount;
 	is >> rWidth;
 	is >> rHeight;
 	is >> rDepth;
 
+	Uint32 total = xCount * yCount; //optimisation
+	std::string str;
+	std::vector<std::string> nameList;
+	nameList.reserve(total);
+
+	//eat until the next line
+	is.ignore(80, '\n');
+
+	//load each region name into the name array
+	for (Uint32 i = 0; i < total; i++) {
+		std::getline(is, str);
+		nameList.push_back(str);
+		std::cout << "Name: " << str << std::endl;
+	}
+
+	//close the map file
+	is.close();
+
+	//generate the array, and load in each region
 	NewMap(xCount, yCount, rWidth, rHeight, rDepth);
 
-	//load the map into the array
+	std::vector<std::string>::iterator it = nameList.begin();
 
+	for (Uint16 i = 0; i < xCount; i++) {
+		for (Uint16 j = 0; j < yCount; j++) {
+			data[i][j].LoadData(it->c_str());
+			it++;
+		}
+	}
 
-	//cleanup
-	is.close();
+	CheckValidData();
 }
 
 void MapUtility::SaveMap(const char* fname) {
@@ -57,11 +82,37 @@ void MapUtility::SaveMap(const char* fname) {
 }
 
 void MapUtility::NewMap(Uint16 x, Uint16 y, Uint16 width, Uint16 height, Uint16 depth) {
-	//TODO
+	if (data != NULL)
+		DeleteMap();
+
+	xCount = x;
+	yCount = y;
+	rWidth = width;
+	rHeight = height;
+	rDepth = depth;
+
+	data = new Region*[xCount];
+	for (Uint16 i = 0; i < xCount; i++) {
+		data[i] = new Region[yCount];
+		for (Uint16 j = 0; j < yCount; j++) {
+			data[i][j].NewData(rWidth, rHeight, rDepth);
+		}
+	}
 }
 
 void MapUtility::DeleteMap() {
+	if (data == NULL)
+		return;
+
 	//TODO
+	for (Uint16 i = 0; i < xCount; i++) {
+		for (Uint16 j = 0; j < yCount; j++) {
+			data[i][j].DeleteData();
+		}
+		delete[] data[i];
+	}
+	delete[] data;
+	data = NULL;
 }
 
 void MapUtility::DrawTo(SDL_Surface* const, Sint16 x, Sint16 y) {
